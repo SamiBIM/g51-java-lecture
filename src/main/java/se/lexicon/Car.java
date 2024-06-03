9bdf40bfcbb1b2620f3120822685f4c1942dcd45
Sami Burhan <samiburhaan@gmail.com>
2024-06-03 21:02:31 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/Car.java
@@ -0,0 +1,16 @@
+//*package se.lexicon;
+
+import org.w3c.dom.ls.LSOutput;
+
+//public class Car {
+
+    //string brandName = "Audi" ;
+    //long registrationNumber = 123456789;
+    //short maxSpeed = 330;
+    //string ownerName ="Sami";
+    //string color = "black" ;//
+
+
+
+
+
