382a5a05155405af28c3a15ef6838eca6d046b14
Sami Burhan <samiburhaan@gmail.com>
2024-06-03 18:11:40 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/FnameLname.java
@@ -0,0 +1,17 @@
+package se.lexicon;
+
+public class FnameLname {
+
+    public static void main(String[] args) {
+
+        String firstName = "Sami";
+        String lastName = "Burhan";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+
+    }
+
+
+
+
+}
