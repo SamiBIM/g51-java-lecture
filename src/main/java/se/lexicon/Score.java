df905fc030a387a008c77a523d16bd1bd1c857cf
Sami Burhan <samiburhaan@gmail.com>
2024-06-03 21:00:55 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/Score.java
@@ -0,0 +1,23 @@
+package se.lexicon;
+
+public class Score {
+
+    public static void main(String[] args) {
+
+        int score = 70;
+        if (score >= 65) {
+            System.out.println("Hurray,You passed!");
+
+        }
+
+        Else if (score >=55 && score <= 64) {
+            System.out.println("You are almost there!");
+        }
+
+        Else
+        {
+            System.out.println("Sorry, you did not pass!");
+        }
+    }
+
+}
