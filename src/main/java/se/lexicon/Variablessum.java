a72cd03933916667acc97eba38867e7695819a39
Sami Burhan <samiburhaan@gmail.com>
2024-06-03 18:07:50 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/Variablessum.java
@@ -0,0 +1,13 @@
+package se.lexicon;
+
+public class Variablessum {
+
+    public static void main(String[] args) {
+
+        double number1 = 2.5;
+        double number2 = 2.5;
+        double result = number1 + number2;
+
+        System.out.println(result);
+    }
+}
