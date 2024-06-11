dab1361cd2d38294f564957e7c2e7f662ef67210
Sami Burhan <samiburhaan@gmail.com>
2024-06-11 20:55:24 +0200

    Added if condition in case negative numbers entered.

--- src/main/java/se/lexicon/sami/Currencyconverter2.java
+++ src/main/java/se/lexicon/sami/Currencyconverter2.java
@@ -18,44 +18,63 @@ public class Currencyconverter2 {
         Date now = new Date();
 
 
-        while (user != 0) {
+        while (user != 0)
+        //while (user >=1);
+        {
 
 
-            if (user == 1){
-                System.out.println("Please enter amount in SEK");
-            double SEK = input.nextInt();
-            double USD = SEK * 0.09524683;
-                System.out.println("The Kronor amount of " + SEK + " = " + USD + " USD "+ now);
-            break;
-            }
-
-        else if (user == 2){
-                System.out.println("Please enter amount in USD");
-            double USD = input.nextInt();
-            double SEK = USD * 10.499152;
-                System.out.println("The USD amount of " + USD + " = " + SEK + " Kronor "+ now);
-                break;
-        }
-            else if (user == 3) {
+            if (user == 1) {
                 System.out.println("Please enter amount in SEK");
                 double SEK = input.nextInt();
-                double Euro = SEK * 0.088680857;
-                System.out.println("The Kronor amount of " + SEK + " = " + Euro + " Euro "+ now);
-                break;
+
+                if (SEK <= 0) {
+                    System.out.println("Error: Zero or Negative amount entered.");
+                } else {
+                    double USD = SEK * 0.09524683;
+                    System.out.println("The Kronor amount of " + SEK + " = " + USD + " USD " + now);
+                    break;
+                }
+            } else if (user == 2) {
+                System.out.println("Please enter amount in USD");
+                double USD = input.nextInt();
+
+                if (USD <= 0) {
+                    System.out.println("Error: Zero or Negative amount entered.");
+                } else {
+                    double SEK = USD * 10.499152;
+                    System.out.println("The USD amount of " + USD + " = " + SEK + " Kronor " + now);
+                    break;
+                }
             }
-            else if (user == 4) {
-                System.out.println("Please enter amount in Euro");
-                double Euro = input.nextInt();
-                double SEK = Euro * 11.275824;
-                System.out.println("The Euro amount of " + Euro + " = " + SEK + " kronor "+ now);
-                break;
-            }
-        else {
-                System.out.println("Invalid entry");
-                break;
+
+            else if (user == 3) {
+                    System.out.println("Please enter amount in SEK");
+                    double SEK = input.nextInt();
+                    if (SEK <= 0) {
+                        System.out.println("Error: Zero or Negative amount entered.");
+                    }
+            else {
+                        double Euro = SEK * 0.088680857;
+                        System.out.println("The Kronor amount of " + SEK + " = " + Euro + " Euro " + now);
+                        break;
+                    }
+                } else if (user == 4) {
+                    System.out.println("Please enter amount in Euro");
+                    double Euro = input.nextInt();
+                    if (Euro <= 0) {
+                        System.out.println("Error: Zero or Negative amount entered.");
+                    }
+                    else {
+                        double SEK = Euro * 11.275824;
+                        System.out.println("The Euro amount of " + Euro + " = " + SEK + " kronor " + now);
+                        break;
+                    }
+                } else {
+                    System.out.println("Invalid entry");
+                    break;
+                }
+
             }
 
         }
-
     }
-}
