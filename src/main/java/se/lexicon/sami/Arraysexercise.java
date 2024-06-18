7f32d19f0faeca9e816b83173f01849e86cbf46f
Sami Burhan <samiburhaan@gmail.com>
2024-06-18 22:00:06 +0200

    Edited the class name and fixed importing errors

--- src/main/java/se/lexicon/sami/Arraysexercise.java
+++ src/main/java/se/lexicon/sami/Arraysexercise.java
@@ -1,17 +1,19 @@
 package se.lexicon.sami;
 
-public class Arrays {
+import java.util.Arrays;
+
+public class Arraysexercise {
 
     public static void main(String[] args) {
         exercise1();
         exercise2();
-        //exercise3();
+        exercise3();
         exercise4();
 
     }
 
     public static void exercise1() {
-
+        System.out.println("--------Exercise1---------");
         int[] arrayex1 = {1, 2, 3};
         //missing a line I think
         System.out.println(arrayex1);
@@ -19,8 +21,9 @@ public class Arrays {
 
 
     public static void exercise2() {
+        System.out.println("--------Exercise2---------");
         int[] arr = {1, 2, 5, 4};
-        int element = 2;
+        int element = 5;
         int index = indexOf(arr, element);
 
         System.out.println("Index position of number " + element + " is: " + index);
@@ -37,16 +40,18 @@ public class Arrays {
 
     }
 
-    /*public static void exercise3() {
+    public static void exercise3() {
+        System.out.println("--------Exercise3---------");
         String[] strArr = {"Paris", "London", "New York", "Stockholm"};
         System.out.println("String array: " + Arrays.toString(strArr));
-        //Arrays.sort(strArr);
+        Arrays.sort(strArr);
         System.out.println("Sorted string array: " + Arrays.toString(strArr));
-    }*/
+    }
 
     public static void exercise4() {
+        System.out.println("--------Exercise4---------");
         int[] firstArr = {1, 15, 20};
-        int[] secondArr = java.util.Arrays.copyOf(firstArr,firstArr.length);
+        int[] secondArr = Arrays.copyOf(firstArr,firstArr.length);
         System.out.println("Elements from first array: " + Arrays.toString(firstArr));
         System.out.println("Elements from second array: " + Arrays.toString(secondArr));
     }
